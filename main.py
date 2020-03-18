07e5463513a80ab2f4ced8f86a356c054501f6f1
lostsquirrel <songli1988@gmail.com>
2020-03-18 15:36:22 +0800

    try download from taobao first

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import json
 import os
+import re
 from pathlib import Path
 from typing import Any
 
+from aiofile import AIOFile
 import couchdb
 import tornado.ioloop
 import tornado.web
 from tornado import httputil
 from tornado.httpclient import AsyncHTTPClient
 
-pkg_base = os.getenv("ORIGIN_SOURCE", "https://registry.npmjs.org")
+pkg_base_npm = "https://registry.npmjs.org"
+pkg_base_custom = os.getenv("ORIGIN_SOURCES", "https://registry.npm.taobao.org")
 mirror_address = os.getenv("MIRROR_ADDRESS", "https://npm.lisong.pub")
 pkg_base_path = Path("/data/npm/_pkg")
-
+pattern = r"-(\d+\.){2}\d+.*tgz$"
+re_suffix = re.compile(pattern)
 
 class MataHandler(tornado.web.RequestHandler):
     def __init__(self, application: "Application", request: httputil.HTTPServerRequest, **kwargs: Any):
@@ -21,13 +25,13 @@ class MataHandler(tornado.web.RequestHandler):
         self.db = couch['registry']
 
     def get(self, meta_id):
-        print(meta_id)
+        print("query meta {}".format(meta_id))
 
         data = self.db[meta_id]
 
         for k, v in data["versions"].items():
             old_pkg_url = v["dist"]["tarball"]
-            prefix = old_pkg_url.replace(pkg_base, "")[1:]
+            prefix = old_pkg_url.replace(pkg_base_npm, "")[1:]
             v["dist"]["tarball"] = "{}/_pkg/{}".format(mirror_address, prefix)
             # print(k, v["name"], v["dist"]["tarball"])
         self.set_header('Content-Type', 'application/json;charset=UTF-8')
@@ -38,23 +42,45 @@ class MataHandler(tornado.web.RequestHandler):
 class PackageHandler(tornado.web.RequestHandler):
 
     async def get(self, pkg_url):
-        print(pkg_url)
 
-        pkg_url_full = "{}/{}".format(pkg_base, pkg_url)
+        async def download_success(req, file_path: str, content: bytes):
+            async with AIOFile(file_path, 'wb') as afp:
+                await afp.write(content)
+                await afp.fsync()
+                req.write(content)
+                req.set_header('Content-Type', 'application/octet-stream')
+                await req.finish()
+
+        print("fetch package {}".format(pkg_url))
+
         pkg_path = pkg_base_path / pkg_url
         if not pkg_path.parent.exists():
             pkg_path.parent.mkdir(parents=True)
 
         http_client = AsyncHTTPClient()
+
+        pkg_url_custom = None
         try:
-            r = await http_client.fetch(pkg_url_full, request_timeout=600.0)
+
+            results = re.search(re_suffix,pkg_url)
+            if results is None:
+                raise Exception("version not recognised")
+            pkg_id = pkg_url.split("/-/")[0]
+            pkg_url_custom = "{}/{}/download/{}{}".format(pkg_base_custom, pkg_id, pkg_id, results.group())
+            r = await http_client.fetch(pkg_url_custom)
         except Exception as e:
-            print("Error: %s" % e)
+            print("fetch {} failed: {}".format(pkg_url_custom, e))
+            pkg_url_npm = "{}/{}".format(pkg_base_npm, pkg_url)
+            try:
+                r = await http_client.fetch(pkg_url_npm, request_timeout=300.0)
+            except Exception as e:
+                print("fetch {} failed: {}".format(pkg_url_npm, e))
+            else:
+                await download_success(self, str(pkg_path), r.body)
         else:
-            pkg_path.write_bytes(r.body)
-            self.set_header('Content-Type', 'application/octet-stream')
-            self.write(pkg_path.read_bytes())
-        await self.finish()
+            await download_success(self, str(pkg_path), r.body)
+
+
 
 
 def make_app():
--- test.py
+++ test.py
@@ -1,6 +1,55 @@
-import  unittest
+import re
+import time
+import unittest
+
+from tornado import httpclient
+from tornado.httpclient import AsyncHTTPClient
+
 
 class Test(unittest.TestCase):
 
     def test_pkg_conf(self):
         pass
+
+    def test_fetch_head(self):
+        http_client = httpclient.HTTPClient()
+
+        try:
+            r = http_client.fetch(
+                "https://registry.npm.taobao.org/@types/babel__template/-/babel__template-7.0.2.tgz", method="HEAD")
+            print(r.code, r.body)
+        except httpclient.HTTPError as e:
+            # HTTPError is raised for non-200 responses; the response
+            # can be found in e.response.
+            print("HTTP Error: " + str(e))
+            print(dir(e))
+            print(e.code)
+            print(type(e.code))
+        except Exception as e:
+            # Other errors are possible, such as IOError.
+            print("Error: " + str(e))
+
+        try:
+            r = http_client.fetch(
+                "https://registry.npm.taobao.org/@types/babel__template/-/babel__template-7.0.2.tgz")
+            print(r.code, r.body)
+        except httpclient.HTTPError as e:
+            # HTTPError is raised for non-200 responses; the response
+            # can be found in e.response.
+            print("HTTP Error: " + str(e))
+            print(dir(e))
+            print(e.code)
+            print(type(e.code))
+        except Exception as e:
+            # Other errors are possible, such as IOError.
+            print("Error: " + str(e))
+
+
+    def test_split(self):
+        "\.\d+\.\d+\.tgz$"
+        pattern = r"-(\d+\.){2}\d+.*tgz$"
+        re_suffix = re.compile(pattern)
+        # print("/@types/babel__template/-/babel__template-7.0.2.tgz".split("/-/"))
+        results = re.search(re_suffix, "cheerio/-/cheerio-1.0.0-rc.3.tgz")
+        if results is not None:
+            print(results.group())
